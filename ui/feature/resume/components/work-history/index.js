c8132ca48330ba7b337b685cadb559396ee4b34e
Michael J. Ryan <tracker1@gmail.com>
2016-03-27 19:22:50 -0700

    add work-history in progress

--- ui/feature/resume/components/work-history/index.js
+++ ui/feature/resume/components/work-history/index.js
@@ -1,27 +1,39 @@
-import Roughneck1 from '';
+import Job001 from './1994-12-self-roughneck';
+//import Job002 from './1999-02-allegro';
+//import Job003 from './1999-05-creative-island-iqorder';
+//import Job004 from './1999-12-big-modis';
+//import Job005 from './2000-11-ucpm';
+//import Job006 from './2001-05-dms';
+import Job007 from './2003-06-inxsol';
+//import Job008 from './2003-06-travel-center';
+import Job009 from './2007-01-datafusion';
+import Job010 from './2008-06-uop-apollo-mdi';
+import Job011 from './2009-03-solusionstream';
+import Job012 from './2009-06-wells-comsys';
+import Job013 from './2010-07-usair-comsys';
+import Job014 from './2011-02-inxsol';
+import Job015 from './2011-11-ccn';
+import Job016 from './2013-05-usair-vold';
+import Job017 from './2013-08-godaddy';
+import Job018 from './2014-07-ccn';
+import Job019 from './2015-05-inxsol';
+import Job020 from './2015-09-aexp-intraedge';
 
 export default function renderWorkHistory() {
   return <div>
-      
-      
-        
-
-        
-        
-
-
-        
-
-
-
+    <Job020 />
+    <Job019 />
+    <Job018 />
+    <Job017 />
+    <Job016 />
+    <Job015 />
+    <Job014 />
+    <Job013 />
+    <Job012 />
+    <Job011 />
+    <Job010 />
+    <Job001 />
+    <Job009 />
+    <Job007 />
   </div>;
 }
-
-  // OLD HISTORY
-  /*
-
-
-                
-        
-
-  */
